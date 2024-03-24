db4caec470a1af0b208d38b97634227caedd5ecf
marvenmalope <marvenmalope2003@gmail.com>
2024-03-24 15:39:58 +0200

    Displayed Products inside an Arraylist

--- prototype/src/main/java/sanienterprise/prototype/model/Product.java
+++ prototype/src/main/java/sanienterprise/prototype/model/Product.java
@@ -8,4 +8,14 @@ public class Product {
     private String name;
     private String price;
     private String imgLink;
+
+    public Product() {
+    }
+
+    public Product(String productID, String name, String price, String imgLink) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.imgLink = imgLink;
+    }
 }
--- prototype/src/main/java/sanienterprise/prototype/rest/ProductController.java
+++ prototype/src/main/java/sanienterprise/prototype/rest/ProductController.java
@@ -1,5 +1,7 @@
 package sanienterprise.prototype.rest;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +13,31 @@ import sanienterprise.prototype.model.Product;
 
 @Controller
 @RequestMapping("/products")
-@SessionAttributes("product")
+@SessionAttributes("productList")
 public class ProductController {
 
     @ModelAttribute
     public void addProductToModel(Model model) {
 
-        Product product = new Product();
+        ArrayList<Product> productList = new ArrayList<>();
+            
+        productList.add(new Product("CH01", "Chair", "R1000", "/images/img1.jpg"));
+        productList.add(new Product("TB02", "Table", "R2000", "/images/img2.jpg"));
+        productList.add(new Product("LM03", "Lamp", "R3000", "/images/img3.jpg"));
+        productList.add(new Product("BS04", "Bookshelf", "R4000", "/images/img4.jpg"));
+        productList.add(new Product("SF05", "Sofa", "R5000", "/images/img5.jpg"));
+        productList.add(new Product("DK06", "Desk", "R6000", "/images/img6.jpg"));
+        productList.add(new Product("MR07", "Mirror", "R7000", "/images/img7.jpg"));
+        productList.add(new Product("BD08", "Bed", "R8000", "/images/img8.jpg"));
+        productList.add(new Product("CB09", "Cabinet", "R9000", "/images/img9.jpg"));
+        productList.add(new Product("RG10", "Rug", "R100000", "/images/img10.jpg"));
 
-        product.setProductID("STG");
-        product.setName("Multi Layered Seat");
-        product.setPrice("R999.99");
-        product.setImgLink("/images/img1.jpg");
+        model.addAttribute("productList", productList);
+    }
 
-        model.addAttribute("product", product);
+    @ModelAttribute(name = "productList")
+    public ArrayList<Product> getProductList() {
+        return new ArrayList<Product>();
     }
 
     @ModelAttribute(name = "product")
