a5285c7f0db1951b718cad3beb6ff848d366b358
marvenmalope <marvenmalope2003@gmail.com>
2024-03-29 10:45:30 +0200

    added project
    Artifact Group: com.sanienterprise
    Artifact ID: dawn

---
+++ dawn/src/test/java/com/sanienterprise/dawn/DawnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanienterprise.dawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
