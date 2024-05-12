499bc50bb95b0fa55ed2bc45823dcb822396bcef
marvenmalope <marvenmalope2003@gmail.com>
2024-05-12 15:00:17 +0200

    yeet

--- dawn/src/main/java/com/sanienterprise/dawn/persistence/entity/Account.java
+++ dawn/src/main/java/com/sanienterprise/dawn/persistence/entity/Account.java
@@ -8,8 +8,6 @@ import org.hibernate.annotations.DynamicUpdate;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
